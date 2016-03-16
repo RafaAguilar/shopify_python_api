61bcd5e675306b26e4512faab539a04f9d4db37e
Rafael Aguilar <rafael.aguilar.1987@gmail.com>
2016-03-15 23:46:31 -0430

    Adding checkout the capability of use search
    Taken from customer.py

--- shopify/resources/checkout.py
+++ shopify/resources/checkout.py
@@ -1,5 +1,20 @@
 from ..base import ShopifyResource
-
+from shopify import mixins
 
 class Checkout(ShopifyResource):
-    pass
+
+    @classmethod
+    def search(cls, **kwargs):
+        """
+        Search for checkouts matching supplied query
+
+        Args:
+           order: Field and direction to order results by (default: last_order_date DESC)
+           query: Text to search for checkouts
+           page: Page to show (default: 1)
+           limit: Amount of results (default: 50) (maximum: 250)
+           fields: comma-seperated list of fields to include in the response
+        Returns:
+           An array of checkouts.
+        """
+        return cls._build_list(cls.get("search", **kwargs))
